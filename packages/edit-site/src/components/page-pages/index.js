52dd37ff2336c629d384a99293b2c3b4c1220ab7
André Maneiro <583546+oandregal@users.noreply.github.com>
2023-10-13 13:41:06 +0200

    Clean up

--- packages/edit-site/src/components/page-pages/index.js
+++ packages/edit-site/src/components/page-pages/index.js
@@ -136,7 +136,7 @@ export default function PagePages() {
 					);
 				},
 				type: 'set',
-				setList: authors, // Elements should have id and name as properties. They should be unique.
+				setList: authors,
 				enableColumnFiltering: true,
 			},
 			{
